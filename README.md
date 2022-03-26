b85b022e78905b79b24fc5bb4012bab9135686c8
Alejandro V <luisalejandrovm@hotmail.com>
2022-03-25 20:32:21 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # download_videos_yt
+[![miniatura][miniatura]](https://youtu.be/gDiSwTOHcXk)
+Turning on modules...
+
+[miniatura]: https://raw.githubusercontent.com/avmmodules/download_videos_yt/main/img/miniatura.png
---
+++ download_videos_yt.py
@@ -0,0 +1,15 @@
+'''
+    Description:
+    Download YouTube video's with python.
+
+    Author: AlejandroV
+    Version: 1.0
+    Video: https://youtu.be/gDiSwTOHcXk
+'''
+from pytube import YouTube
+
+# Your url video of YouTube
+url = ""
+
+# Download
+YouTube(url).streams.first().download()
