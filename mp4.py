204a3f8924de29ffc476429d8023b355be5c6e3c
Alejandro V <luisalejandrovm@hotmail.com>
2023-03-16 01:41:23 -0600

    Username

--- mp4.py
+++ mp4.py
@@ -1,9 +1,7 @@
 '''
-    Description:
-    Download YouTube video's with python.
-
-    Author: AlejandroV
-    Version: 1.0
+    Description: Download YouTube video's with python.
+    Author: aulerjbailey
+    Version: 1.0.0
     Video: https://youtu.be/gDiSwTOHcXk
 '''
 from pytube import YouTube
